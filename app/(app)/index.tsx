2e86eb3cb9a578ff84192cf8ee015f926b1f4793
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-07 15:09:04 -0500

    project files cleanup

---
+++ app/(app)/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "../../components/Themed";
+
+const IndexView = () => {
+    return (
+        <View>
+           <Text>INDEX VIEW</Text>
+        </View>
+    );
+};
+
+export default IndexView;
