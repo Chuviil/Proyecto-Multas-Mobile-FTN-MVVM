d13cc9e45091f03bcd92a34b4cf3e4a4ae8f132c
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-09 12:16:21 -0500

    added Multa model

---
+++ models/Multa.ts
@@ -0,0 +1,13 @@
+export type Multa = {
+    multaId: number,
+    monto: number,
+    razon: string,
+    emitida: Date,
+}
+
+export type MultaResDTO = {
+    MultaId: number,
+    Monto: number,
+    Razon: string,
+    Emitida: Date,
+}
