fb9b3d924044c96ede6be95aa81d1c619f0323fc
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-09 12:17:03 -0500

    created ViewModel for Multa and linked with view

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,21 +1,61 @@
-import React from 'react';
-import {Text, View} from '../../components/Themed';
-import {useSession} from '../../ctx';
+import React, {useEffect, useState} from 'react';
+import MultasView from "../../views/MultasView";
+import {Multa, MultaResDTO} from "../../models/Multa";
+import AxiosClient from "../../AxiosClient";
+import {useSession} from "../../ctx";
+import {AxiosResponse} from "axios";
 
-export default function Index() {
-    const {signOut, user} = useSession();
+const MultasViewModel = () => {
+    const {user, userLoading} = useSession();
 
-    return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Hola {user.nombre}</Text>
-            <Text
-                onPress={() => {
-                    // The `app/(app)/_layout.tsx` will redirect to the sign-in screen.
-                    signOut();
-                }}
-            >
-                Sign Out
-            </Text>
-        </View>
-    );
+    const [multas, setMultas] = useState<Multa[]>([]);
+    const [isFetching, setIsFetching] = useState<boolean>(false);
+    const [refreshing, setRefreshing] = useState<boolean>(false);
+
+    const handleCardPress = (id: number) => {
+        console.log(id);
+    }
+
+    const fetchMultas = async () => {
+        setIsFetching(true);
+
+        try {
+            const response : AxiosResponse<MultaResDTO[]>= await AxiosClient.get(`/Ayudante/${user.idBanner}/multas`);
+            setMultas(response.data.map((multa) => ({
+                multaId: multa.MultaId,
+                monto: multa.Monto,
+                emitida: multa.Emitida,
+                razon: multa.Razon
+            })));
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setIsFetching(false);
+        }
+    }
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        fetchMultas();
+        setRefreshing(false);
+    }
+
+    useEffect(() => {
+        fetchMultas();
+    }, [userLoading]);
+
+    const viewModel = {
+        multas,
+        isFetching,
+        usuario: user,
+        refreshing,
+        onRefresh,
+        handleCardPress,
+    }
+
+    if (!user) return null;
+
+    return <MultasView viewModel={viewModel}/>
 }
+
+export default MultasViewModel;
