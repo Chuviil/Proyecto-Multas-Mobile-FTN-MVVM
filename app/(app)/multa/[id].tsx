a4cb2e2e86815c6881b5ff988dc309f34dc181d0
Sebastian Quito <josue.quito@udla.edu.ec>
2024-01-10 20:00:16 -0500

    added fetching in multa details screen

--- app/(app)/multa/[id].tsx
+++ app/(app)/multa/[id].tsx
@@ -1,17 +1,38 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import MultasDetailsView from "../../../views/MultasDetailsView";
-import {Multa} from "../../../models/Multa";
+import {Multa, MultaResDTO} from "../../../models/Multa";
 import {useSession} from "../../../ctx";
 import AxiosClient from "../../../AxiosClient";
 import {router, useLocalSearchParams} from "expo-router";
+import {AxiosResponse} from "axios";
 
 const MultasDetailsViewModel = () => {
 
     const {id} = useLocalSearchParams();
-    const {session, user} = useSession();
+    const {session, user, userLoading} = useSession();
 
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [multa, setMulta] = useState<Multa>({multaId: 0, razon: "", emitida: new Date(), monto: 0});
+    const [multa, setMulta] = useState<Multa>(
+        {multaId: 0, razon: "", emitida: new Date(), monto: 0}
+    );
+
+    const fetchMulta = async () => {
+        try {
+            const response : AxiosResponse<MultaResDTO>= await AxiosClient.get(`/Multa/${id}`);
+            setMulta({
+                multaId: response.data.MultaId,
+                monto: response.data.Monto,
+                emitida: response.data.Emitida,
+                razon: response.data.Razon
+            });
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        fetchMulta();
+    }, [userLoading])
 
     const handlePagar = async () => {
         try {
